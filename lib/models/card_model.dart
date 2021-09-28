b8218f7655f54c024b7c2635b827e299cfd566f1
AyobamiAdebesin <froshofficial@gmail.com>
2021-09-28 09:01:55 +0100

    fixed some bugs

--- lib/models/card_model.dart
+++ lib/models/card_model.dart
@@ -28,7 +28,7 @@ var cardData = [
     "user": "Ayobami",
     "cardNumber": "**** **** **** 1425",
     "cardExpired": "03-01-2023",
-    "cardType": "..assets/images/mastercard_logo.png",
+    "cardType": "assets/images/mastercard_logo.png",
     "cardBackground": 0xFF1E1E99,
     "cardElementTop": "assets/svg/ellipse_top_pink.svg",
     "cardElementBottom": "assets/svg/ellipse_bottom_pink.svg"
@@ -37,7 +37,7 @@ var cardData = [
     "user": "Ayobami",
     "cardNumber": "**** **** **** 8287",
     "cardExpired": "03-01-2025",
-    "cardType": "..assets/images/mastercard_logo.png",
+    "cardType": "assets/images/mastercard_logo.png",
     "cardBackground": 0xFFFF70A3,
     "cardElementTop": "assets/svg/ellipse_top_blue.svg",
     "cardElementBottom": "assets/svg/ellipse_bottom_blue.svg"
